e577662cae351c2539215e13e2de85fd2bcee59d
sohun-cs <asaduzzamansohun5@gmail.com>
2025-08-23 16:23:44 +0600

    Added Global error handler and AppError class

---
+++ src/app/middlewares/AppError.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+
+class AppError extends Error {
+
+    public statusCode: number
+
+    constructor(err: any, statusCode: number, message: string, stack = '') {
+        super(message) // Basically the default js's Error class message
+
+        this.statusCode = statusCode;
+
+        if (stack) {
+            this.stack = stack
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+
+export default AppError;
---
+++ src/app/middlewares/global.error.handler.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextFunction, Request, Response } from "express";
+import { envVars } from "../configs/env.config";
+import AppError from "./AppError";
+
+
+
+export const GlobalErrorHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
+
+    let statusCode = 500;
+    let message = 'Something wend wrong.'
+
+    if (err instanceof AppError) {
+        statusCode = err.statusCode
+        message = err.message
+    }
+    else if(err instanceof Error){
+        statusCode = 500
+        message= err.message
+    }
+
+    res.status(statusCode).json({
+        success: false,
+        message: message,
+        err,
+        stack: envVars.NODE_ENV === 'development' ? err.stack : null
+    })
+
+}
